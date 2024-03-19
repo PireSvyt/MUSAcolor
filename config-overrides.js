be5e7ebdee80dbf5bc5fb2b595b9969cea39dcb5
Pire Svyt <savoyat.p@gmail.com>
2024-03-19 07:43:10 +0100

    attempt

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,13 @@
 module.exports = function override(config, env) {
-  //console.log('config-overrides config',config)
-  console.log('config-overrides env',env)
-  let loaders = config.resolve
-  loaders.fallback = {
-    path: require.resolve('path-browserify'),
-    crypto: require.resolve('crypto-browserify'),
-    os: require.resolve('os-browserify/browser'),
-    buffer: require.resolve('buffer/'),
-    stream: require.resolve('stream-browserify'),
-    vm: require.resolve('vm-browserify'),
-  }
-  return config;
-}
+    console.log("config-overrides");
+    let loaders = config.resolve;
+    loaders.fallback = {
+        path: require.resolve("path-browserify"),
+        crypto: require.resolve("crypto-browserify"),
+        os: require.resolve("os-browserify/browser"),
+        buffer: require.resolve("buffer/"),
+        stream: require.resolve("stream-browserify"),
+        vm: require.resolve("vm-browserify"),
+    };
+    return config;
+};
